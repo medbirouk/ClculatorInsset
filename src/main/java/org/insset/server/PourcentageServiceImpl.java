5db4aa07b5145a819938c7506643cb50dcc8c892
medbirouk <63082912+medbirouk@users.noreply.github.com>
2024-10-27 13:51:19 +0100

    Update PourcentageServiceImpl.java

--- src/main/java/org/insset/server/PourcentageServiceImpl.java
+++ src/main/java/org/insset/server/PourcentageServiceImpl.java
@@ -26,4 +26,11 @@ public class PourcentageServiceImpl extends RemoteServiceServlet implements Pour
         
         return prixFinal / (1 - pourcentage / 100);
     }
+    @Override
+    public Double effectuerDivision(int premierNombre, int deuxiemeNombre) throws IllegalArgumentException {
+        if (deuxiemeNombre == 0) {
+            throw new IllegalArgumentException("Division par zéro n'est pas autorisée.");
+        }
+        return (double) premierNombre / deuxiemeNombre;
+    }
 }
