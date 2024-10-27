cdeac21e244449a8d15e2a89d8201bd6518344c2
medbirouk <63082912+medbirouk@users.noreply.github.com>
2024-10-27 13:50:12 +0100

    Update PourcentageServiceAsync.java

--- src/main/java/org/insset/client/service/PourcentageServiceAsync.java
+++ src/main/java/org/insset/client/service/PourcentageServiceAsync.java
@@ -8,4 +8,6 @@ public interface PourcentageServiceAsync {
     void calculerPourcentage(double prixDepart, double pourcentage, AsyncCallback<CalculPourcentageResult> callback);
 
     void calculerPrixDepart(double prixFinal, double pourcentage, AsyncCallback<Double> callback);
+
+    void effectuerDivision(int premierNombre, int deuxiemeNombre, AsyncCallback<Double> callback);
 }
